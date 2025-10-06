e94d8c3d500a1765c15fd8e51cd797fcb33b50c8
Tom Gollick <thomasgollick@gmail.com>
2025-10-06 09:48:11 +0100

    Updating the jobs route to accept additional search parameters for filtered searching

--- frontend/src/app/api/jobs/route.ts
+++ frontend/src/app/api/jobs/route.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/index";
 import { jobs } from "@/db/schema";
 import { NextResponse } from "next/server";
 import { sql } from "drizzle-orm";
+import { and, gte, lte, ilike, or } from "drizzle-orm";
 
 export async function GET(request: Request) {
   try {
@@ -9,15 +10,43 @@ export async function GET(request: Request) {
 
     const page = parseInt(searchParams.get("page") || "1", 10);
     const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const location = searchParams.get("location")
+    const searchTerm = searchParams.get("searchTerm")
+    const minSalary = parseInt(searchParams.get("minSalary") || "0");
+    const maxSalary = parseInt(searchParams.get("maxSalary") || "0");
 
     const validPage = Math.max(1, page);
     const validLimit = Math.min(Math.max(1, limit), 15);
 
     const offset = (validPage - 1) * validLimit;
+  
+    const conditions = []
+
+    if(location) {
+      conditions.push(ilike(jobs.location, `%${location}%`));
+    }
+
+    if (searchTerm) {
+      conditions.push(
+        or(
+          ilike(jobs.title, `%${searchTerm}%`),
+          ilike(jobs.description, `%${searchTerm}%`)
+        )
+      );
+    }
+
+    if(minSalary > 0) {
+      conditions.push(gte(jobs.salaryMin, minSalary));
+    }
+
+    if(maxSalary > 0) {
+      conditions.push(gte(jobs.salaryMax, maxSalary));
+    }
 
     const jobData = await db
       .select()
       .from(jobs)
+      .where(conditions.length > 0 ? and(...conditions) : undefined)
       .limit(validLimit)
       .offset(offset)
       .orderBy(jobs.postedAt);
